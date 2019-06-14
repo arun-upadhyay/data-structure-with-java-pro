aaf739916af8942d18be35a1a9461b6b72a9407f
Arun Upadhyay <arunnnet@gmail.com>
2019-06-13 20:31:54 -0400

    Average value

--- src/Array/Average.java
+++ src/Array/Average.java
@@ -1,15 +1,17 @@
 package Array;
 
 public class Average {
-    private  static  int  findAverage(int [] arr){
+    private  static  float  findAverage(int [] arr){
         int sum = 0;
         for(int val : arr){
             sum = sum + val;
         }
-        return sum/arr.length;
+
+        return (float) sum/(float) arr.length;
     }
     public static void main(String[] args) {
-        int [] arr = {12,3,5,3,53,3,};
+        int [] arr = {12,3,5,376,53,3,};
         System.out.println(findAverage(arr));
+
     }
 }
